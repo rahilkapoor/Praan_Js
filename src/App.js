55a4edf03bddf404452d00af8fcf542bc203a2f1
Rahil Kapoor <rahilkapoor2000@gmail.com>
2021-05-27 16:40:24 +0530

    Dataset with filter

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import moment from "moment";
 import Chart from "./Chart";
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "./theme.js";
+import Daychart from "./Daychart.js";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const StyledApp = styled.div`
 	.app__body {
 		color: ${(props) => props.theme.fontColor};
-		margin: 60px;
 		margin-top: 75px;
-		padding: 20px;
+		// width: 80%;
+		// justify-content: center;
+		// text-align: center;
+		// align-content: center;
+		padding: 40px;
 	}
 	.app__header {
 		position: fixed;
@@ -23,6 +28,7 @@ const StyledApp = styled.div`
 		padding: 20px;
 		width: 100%;
 		z-index: 10;
+		box-shadow: 2px 5px 8px #ddd;
 	}
 	h1 {
 		position: fixed;
@@ -39,6 +45,7 @@ const StyledApp = styled.div`
 
 function App() {
 	const [theme, setTheme] = useState("light");
+	const [query, setQuery] = useState("");
 
 	const themetoggler = () => {
 		theme === "light" ? setTheme("dark") : setTheme("light");
@@ -47,9 +54,8 @@ function App() {
 	const [database, setDatabase] = useState([]);
 
 	useEffect(() => {
-		getData();
 		async function getData() {
-			const response = await fetch("test.csv");
+			const response = await fetch("test_dataset_all.csv");
 			const data = await response.text();
 
 			// console.log(data);
@@ -62,7 +68,7 @@ function App() {
 				var string = row[1] + " " + row[2];
 				// console.log(string);
 
-				let m = moment(string, "DD/MM/YYYY hh:mm:ss");
+				let m = moment(string, "YY/MM/DD hh:mm:ss");
 				var date = m.toString().split(" ");
 
 				// var day;
@@ -80,13 +86,20 @@ function App() {
 
 			setDatabase(col);
 		}
+		getData();
 	}, []);
+
+	function search(records) {
+		return records.filter(
+			(row) => row[1].toString().toLowerCase().indexOf(query) > -1
+		);
+	}
 
 	return (
 		<ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
 			<GlobalStyles />
 			<StyledApp>
-				<div className="app__body">
+				<div className="app">
 					<div className="app__header">
 						<h1>Praan Js</h1>
 						<button
@@ -97,13 +110,45 @@ function App() {
 						</button>
 					</div>
 
-					<DataTable data={database} />
-
-					<p>My Chart ❤</p>
-					<Chart />
+					<Router>
+						<div className="app__body">
+							<div>
+								Day Filter :
+								<input
+									type="text"
+									value={query}
+									placeholder="DD MON YYYY"
+									onChange={(e) => {
+										setQuery(e.target.value);
+									}}
+								/>
+							</div>
+							<DataTable data={search(database)} />
+							<nav>
+								<ul>
+									<li>
+										<Link to="/">Comparison Graph</Link>
+									</li>
+									<li>
+										<Link to="/daily">
+											Time-Series Graph
+										</Link>
+									</li>
+								</ul>
+							</nav>
+							<Switch>
+								<Route path="/daily">
+									<Daychart chartdata={search(database)} />
+								</Route>
+								<Route path="/">
+									<p>My Chart ❤</p>
+									<Chart chartdata={search(database)} />
+								</Route>
+							</Switch>
+						</div>
+					</Router>
 				</div>
 			</StyledApp>
-			{/* </GlobalStyles> */}
 		</ThemeProvider>
 	);
 }
--- src/Chart.js
+++ src/Chart.js
@@ -1,55 +1,92 @@
 import { useState, useEffect } from "react";
 import moment from "moment";
-import { Bar } from "react-chartjs-2";
+import { Doughnut, Pie } from "react-chartjs-2";
 
 const Chart = ({ chartdata }) => {
-	const [database, setDatabase] = useState([]);
+	const labels = ["PM 1 Particle", "PM 2.5 Particle", "PM 10 Particle"],
+		data_p1 = [];
 
-	useEffect(() => {
-		getData();
-		async function getData() {
-			const response = await fetch("test.csv");
-			const data = await response.text();
+	var sum_p1 = 0.0,
+		sum_p2 = 0.0,
+		sum_p10 = 0.0;
 
-			// console.log(data);
-			const table = data.split("\n").slice(1);
-
-			// console.log(table);
-			const p1_data = [];
-			const p2_data = [];
-			const chart_label = [];
-			const p10_data = [];
-			table.forEach((record) => {
-				const row = record.split(",");
-				var string = row[1] + " " + row[2];
-				// console.log(string);
-
-				let m = moment(string, "DD/MM/YYYY hh:mm:ss");
-				var date = m.toString().split(" ");
-
-				var month, tarik, year, time;
-				month = date[1];
-				tarik = date[2];
-				year = date[3];
-				time = date[4];
-				row[1] = tarik + " " + month + " " + year;
-				row[2] = time;
-
-				chart_label.push(row[1]);
-				p1_data.push(row[5]);
-				p2_data.push(row[6]);
-				p10_data.push(row[7]);
-			});
-
-			setDatabase([chart_label, p1_data, p2_data, p10_data]);
-			console.log(database);
+	function refactorData() {
+		for (var i = 0; i < chartdata.length; i++) {
+			sum_p1 = sum_p1 + parseFloat(chartdata[i][5]);
+			sum_p2 = sum_p2 + parseFloat(chartdata[i][6]);
+			sum_p10 = sum_p10 + parseFloat(chartdata[i][7]);
+			// data_p1.push(chartdata[i][5]);
+			// data_p2.push(chartdata[i][6]);
+			// data_p3.push(chartdata[i][7]);
 		}
-	}, []);
+		data_p1.push(sum_p1);
+		data_p1.push(sum_p2);
+		data_p1.push(sum_p10);
+	}
+	refactorData();
+
+	// const [database, setDatabase] = useState([]);
+
+	// useEffect(() => {
+	// 	getData();
+	// 	async function getData() {
+	// 		const response = await fetch("test.csv");
+	// 		const data = await response.text();
+
+	// 		// console.log(data);
+	// 		const table = data.split("\n").slice(1);
+
+	// 		// console.log(table);
+	// 		const p1_data = [];
+	// 		const p2_data = [];
+	// 		const chart_label = [];
+	// 		const p10_data = [];
+	// 		table.forEach((record) => {
+	// 			const row = record.split(",");
+	// 			var string = row[1] + " " + row[2];
+	// 			// console.log(string);
+
+	// 			let m = moment(string, "DD/MM/YYYY hh:mm:ss");
+	// 			var date = m.toString().split(" ");
+
+	// 			var month, tarik, year, time;
+	// 			month = date[1];
+	// 			tarik = date[2];
+	// 			year = date[3];
+	// 			time = date[4];
+	// 			row[1] = tarik + " " + month + " " + year;
+	// 			row[2] = time;
+
+	// 			chart_label.push(row[1]);
+	// 			p1_data.push(row[5]);
+	// 			p2_data.push(row[6]);
+	// 			p10_data.push(row[7]);
+	// 		});
+
+	// 		setDatabase([chart_label, p1_data, p2_data, p10_data]);
+	// 		console.log(database);
+	// 	}
+	// }, []);
 
 	return (
 		<div>
-			<Bar
-				data={database}
+			<Doughnut
+				data={{
+					labels: labels,
+					datasets: [
+						{
+							label: "P1",
+							data: data_p1,
+							backgroundColor: [
+								"rgba(255, 99, 132, 0.4)",
+								"rgba(54, 162, 235, 0.4)",
+								"rgba(255, 205, 86, 0.4)",
+							],
+							fill: false,
+							hoverOffset: 4,
+						},
+					],
+				}}
 				width={600}
 				height={500}
 				options={{
